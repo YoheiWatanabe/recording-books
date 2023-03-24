6c96ff9cca1be4bdf2f8df5609ae38546b640508
yoheiwatanabe <yohei.watanabe@aitit.co.jp>
2023-03-25 01:08:15 +0900

    コメントアウトの削除

--- backend/backend/books/views.py
+++ backend/backend/books/views.py
@@ -43,10 +43,3 @@ class StoreList(generics.ListAPIView):
         store_name = query_serializer.validated_data['store_name']
         queryset = Store.objects.filter(store_name__contains=store_name)
         return Response(data=self.get_serializer(queryset, many=True).data)
-
-# __contains=値
-    # def list(self, request, *args, **kwargs):
-    #     """List"""
-    #     query_serializer = StoreQuerySerializer(data=request.query_params)
-    #     query_serializer.is_valid(raise_exception=True)
-    #     return super().retrieve(request, *args, **kwargs)
